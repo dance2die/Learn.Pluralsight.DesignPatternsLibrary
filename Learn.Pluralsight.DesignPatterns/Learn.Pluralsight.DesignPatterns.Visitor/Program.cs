63678c0a8d8f2f122476f6564d3283b63e188592
dance2die <dance2die@hotmail.com>
2015-10-25 15:25:35 -0400

    Implemented "RequiredValidateVisitor"

--- Learn.Pluralsight.DesignPatterns/Learn.Pluralsight.DesignPatterns.Visitor/Program.cs
+++ Learn.Pluralsight.DesignPatterns/Learn.Pluralsight.DesignPatterns.Visitor/Program.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace Learn.Pluralsight.DesignPatterns.Visitor
 {
@@ -11,26 +10,65 @@ namespace Learn.Pluralsight.DesignPatterns.Visitor
 	{
 		public static void Main(string[] args)
 		{
+			Gtfs gtfs = new Gtfs();
+			gtfs.Entities.Add(new Agency {Id = "1", Name="Agency Name"});
+			gtfs.Entities.Add(new FareRules { RouteId = "10"});
+			gtfs.Entities.Add(new Frequencies());
 
+			RequiredValidateVisitor requiredValidateVisitor = new RequiredValidateVisitor();
+			gtfs.Accept(requiredValidateVisitor);
+
+			Console.Read();
 		}
 	}
 
-	public class ValidateVisitor : IVisitor
+	public class RequiredValidateVisitor : ValidateVisitor
 	{
-		public void Visit(FareRules fareRules)
+		private const string ERROR_MESSAGE = "Property value is required";
+
+		public override void Visit(FareRules fareRules)
 		{
-			
+			SetFailedProperties(fareRules);
 		}
 
-		public void Visit(Frequencies frequencies)
+		public override void Visit(Frequencies frequencies)
 		{
-			
+			SetFailedProperties(frequencies);
 		}
 
-		public void Visit(Agency agency)
+		public override void Visit(Agency agency)
 		{
-			
+			SetFailedProperties(agency);
 		}
+
+		private void SetFailedProperties(object obj)
+		{
+			var requiredProperties = GetRequiredProperties(obj.GetType());
+			foreach (PropertyInfo requiredProperty in requiredProperties)
+			{
+				var value = requiredProperty.GetValue(obj);
+				if (value == null)
+					FailedProperties.Add(requiredProperty, ERROR_MESSAGE);
+			}
+		}
+
+		private IEnumerable<PropertyInfo> GetRequiredProperties(Type type)
+		{
+			// http://stackoverflow.com/a/2282254/4035
+			IEnumerable<PropertyInfo> properties = type.GetProperties().Where(
+				property => Attribute.IsDefined(property, typeof(RequiredAttribute)));
+			return properties;
+		}
+	}
+
+	public abstract class ValidateVisitor : IVisitor
+	{
+		public Dictionary<PropertyInfo, string> FailedProperties { get; }
+			= new Dictionary<PropertyInfo, string>();
+
+		public abstract void Visit(Agency agency);
+		public abstract void Visit(FareRules fareRules);
+		public abstract void Visit(Frequencies frequencies);
 	}
 
 	public interface IVisitor
@@ -38,6 +76,18 @@ namespace Learn.Pluralsight.DesignPatterns.Visitor
 		void Visit(Agency agency);
 		void Visit(FareRules fareRules);
 		void Visit(Frequencies frequencies);
+	}
+
+	public class Gtfs : IEntity
+	{
+		public List<IEntity> Entities { get; set; } = new List<IEntity>();
+		public void Accept(IVisitor visitor)
+		{
+			foreach (IEntity entity in Entities)
+			{
+				entity.Accept(visitor);
+			}
+		}
 	}
 
 	public interface IEntity
